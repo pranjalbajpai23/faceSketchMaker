91d4ce712c1b0ebe36c18f591250ba7cff37e869
pranjalbajpai23 <pranjalbajpai400@gmail.com>
2024-03-07 00:14:42 +0530

    UI: Improvement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const pages = [
         {
             text: "Home",
-            route: '/home'
+            route: '/'
         },
         {
             text: "Create a Sketch",
@@ -25,9 +25,9 @@ const Navbar = () => {
             text: "About",
             route: "/about",
         }
-        
+
     ]
-    
+
 
     return <>
         <div className="flex text-black justify-between p-2 bg-gray-300" >
@@ -38,14 +38,15 @@ const Navbar = () => {
                 {
                     pages.map(item => (
 
-                        <li key={item.text}><NavLink to={item.route} className="nav-NavLink px-4 text-black hover:underline">{item.text}</NavLink></li>
+                        <li key={item.text}><NavLink to={item.route} className="nav-NavLink  mx-4 text-black hover:border-b-2 border-black"
+                            style={({ isActive }) => (isActive ? { borderBottom: "2px solid black" } : {})} >{item.text}</NavLink></li>
                     ))
                 }
             </ul>
             <div className="">
-                <button type="button" className="py-2 px-4 mr-4 bg-blue-700 text-white hover:bg-blue-600">Login</button>
-                
-                <button type="button" className="py-2 px-4 mr-4 bg-blue-700 text-white hover:bg-blue-600">Sign Up</button>
+                <button type="button" className="py-2 px-4 mr-4 rounded-md bg-blue-700 text-white hover:bg-blue-600 ">Login</button>
+
+                <button type="button" className="py-2 px-4 mr-4 rounded-md bg-blue-700 text-white hover:bg-blue-600 ">Sign Up</button>
             </div>
 
         </div>
--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -2,14 +2,12 @@ import { Link } from "react-router-dom";
 
 const LandingPage = () => {
     return <>
-        <div className='flex p-4 items-center bg-gray-300'>
+        <div className='flex p-4 items-center bg-gray-300 h-dvh'>
             <div className="">
                 <h1 className="text-3xl font-semibold">Create Forensic Sketch with Ease</h1> <br></br>
                 <p className="font-normal text-base" >Get a simple and convenient method to create face sketches using our intuitive drag-and-drop interface.These sketches can then be compared with images in the criminal database to speed up investigations.</p>
                 <br></br>
-                <div className=" border-4 rounded-md w-fit border-black">
-                    <Link to="/create" type="button" className="py-2 px-4 bg-blue-700 text-white hover:bg-blue-600">Get Started</Link>
-                </div>
+                <Link to="/create" type="button" className="py-2 px-4 bg-blue-700 w-fit rounded-md text-white hover:bg-blue-600">Get Started</Link>
             </div>
             <div className="">
                 <img src="bg.png" className="" alt="Bootstrap Themes" loading="lazy" />
