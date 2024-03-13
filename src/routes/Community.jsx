e4fc7fe97bf2fa689d4e672644ca70f9c9976a60
divya9932 <divyadxt02@gmail.com>
2024-03-13 14:12:56 +0530

    UI: Upload Sketch

--- src/routes/Community.jsx
+++ src/routes/Community.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 const Community=()=>{
     return<>
-    <div className='flex flex-col items-center  h-full'>
+    <div className='flex flex-col items-center h-full  bg-gray-200' >
       <h1 className="text-2xl font-serif mt-40">Discussion Forum</h1>
       <br></br>
       <Link to="https://t.me/+ukSZyyRReno0NjBl" type="button" className="py-2 px-4 bg-blue-600 w-fit rounded-md text-white hover:bg-blue-400 mt-2">Join our Telegram Channel</Link>
--- src/routes/FaceMatching.jsx
+++ src/routes/FaceMatching.jsx
@@ -3,22 +3,28 @@ import axios from 'axios';
 
 function App() {
     const [selectedFile, setSelectedFile] = useState(null);
+    const [sendImage,setSendImage]=useState(null)
     const [matchedImages, setMatchedImages] = useState([]);
 
     const handleFileInputChange = (event) => {
-        setSelectedFile(event.target.files[0]);
+        setSendImage(event.target.files[0]);
+        setSelectedFile(URL.createObjectURL( event.target.files[0]));
+        console.log(selectedFile);
     };
     
     const handleUpload = async () => {
         try {
             const formData = new FormData();
-            formData.append('sketch', selectedFile);
+            formData.append('sketch', sendImage);
 
             const response = await axios.post('http://localhost:5000/upload', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
             });
+
+            
+
             console.log(response.data);
             setMatchedImages(response.data.matched_images);
         } catch (error) {
@@ -28,16 +34,28 @@ function App() {
 
     return (
         <div>
-            <input type="file" onChange={handleFileInputChange} />
-            <button onClick={handleUpload}>Upload</button>
-
-            <div>
-                {matchedImages.map((matchedImage, index) => (
-                    <div key={index}>
-                        <p>Person Name: {matchedImage}</p>
-                        <img src={matchedImage} alt={`Matched image for ${matchedImage}`} />
-                    </div>
-                ))}
+            <div className='flex flex-col  items-center'>
+                <input className='border-black border-2  rounded-md mb-4' type="file" onChange={handleFileInputChange} />
+                <button className='border-black border-2 p-2 rounded-md' onClick={handleUpload}>Upload</button>
+            </div>
+            
+            <div className='flex justify-evenly mt-4'>
+                <div>
+                    <img src={selectedFile} style={{width:"20vw"}} alt="" />
+                    <p className='text-center'>Uploaded Sketch</p>
+                </div>
+                <div style={{backgroundColor:"black", width:"1vh"}} />
+                <div>
+                    {matchedImages.map((matchedImage, index) => (
+                        <div key={index}>
+                            
+                            <img src={matchedImage} alt={`Matched image for ${matchedImage}`} style={{width:"20vw"}} />
+                            <p className='text-center'>Matched Image</p>
+                            <p className='text-center'>Person Name: {matchedImage.match(/([^\\]+)(?=\.\w+$)/)[0]}</p>
+                            <p className='text-center'>Matched %: 90%</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
