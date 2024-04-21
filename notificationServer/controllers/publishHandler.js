cbc111dccf799c407939cc2be2647d6abc71470c
pranjalbajpai23 <pranjalbajpai400@gmail.com>
2024-04-22 01:28:14 +0530

    NOTIFICATION: Whastaap added

--- notificationServer/controllers/publishHandler.js
+++ notificationServer/controllers/publishHandler.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-undef */
 const fs = require("fs");
 const path = require("path");
+//Yaha par add karna hai  code
+//*************************** */
+//*************************** */
+//*************************** */
+//*************************** */
+//*************************** */
 
 const handlePublish = (req, res) => {
   // Path to the file within the modal folder
   const filePath = path.join(__dirname, "../modal/criminal.txt");
-
   // Read the content of the file
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
@@ -15,10 +20,49 @@ const handlePublish = (req, res) => {
     const detail = data.split(",");
     const name = detail[0].trim(); // Trim whitespace characters
     const pic = detail[1].trim();
-
-    return res.status(200).json({ name: name, pic: pic });
+    var link=pic.split("\\");
+    link=link[1];
+    const message = `Hello Officer you have an new notification\n \n Name: ${name}`;
+    console.log(link);
+    const lastPhotoSent = path.join(__dirname, "../modal/sent.txt");
+    fs.readFile(lastPhotoSent, "utf8", (err, data)=>{
+      if (data != link) {
+        phoneNumbers.map((phoneNumber) => {
+          client.messages
+            .create({
+              body: message,
+              from: "whatsapp:+14155238886",
+              mediaUrl: `https://pranjalbajpai23.github.io/faceSketchMaker/public/faces/${link}`,
+              to: `whatsapp:+91${phoneNumber}`,
+            })
+            .then((message) => {
+              console.log("Message sent successfully", message);
+              if (phoneNumber == "6386674090") {
+                fs.writeFile(lastPhotoSent, link, (err) => {
+                  if (err) {
+                    console.error("Error saving data:", err);
+                  }
+                  console.log("SENT Link saved successfully:", link);
+                });
+                return res.status(200).json({ name: name, pic: pic });
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(500).json({ message: "Internal server error" });
+            });
+        });
+      } else {
+        return res.status(200).json({ name: name, pic: pic });
+      }
+    })
+    
   });
 };
+
+
+
+
 
 module.exports = {
   handlePublish,
